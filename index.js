1f65fda2815eafb9d5f95f6065bddc4d1b63e999
Faisal Almail <faisalyounis90@gmail.com>
2025-10-16 16:14:12 +0300

    Search part finished
    Next is to use the local store to add movies to the list
    
    add pagination

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
 const form = document.getElementById("search-form")
 const searchTerm = document.getElementById("search-term")
 const dummyMovies= [{
-    "Title": "Men in Black II",
-    "Year": "2002",
-    "Rated": "PG-13",
-    "Released": "03 Jul 2002",
-    "Runtime": "88 min",
-    "Genre": "Action, Adventure, Comedy",
-    "Director": "Barry Sonnenfeld",
-    "Writer": "Lowell Cunningham, Robert Gordon, Barry Fanaro",
-    "Actors": "Tommy Lee Jones, Will Smith, Rip Torn",
-    "Plot": "Agent J is sent to find Agent K and restore his memory after the re-appearance of a case from K's past.",
-    "Language": "English",
-    "Country": "United States",
-    "Awards": "4 wins & 16 nominations total",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMTMxNDA0NT
-    "Ratings": [
-        {
-            "Source": "Internet Movie Database",
-            "Value": "6.2/10"
-        },
-        {
-            "Source": "Rotten Tomatoes",
-            "Value": "38%"
-        },
-        {
-            "Source": "Metacritic",
-            "Value": "49/100"
-        }
-    ],
-    "Metascore": "49",
-    "imdbRating": "6.2",
-    "imdbVotes": "421,618",
-    "imdbID": "tt0120912",
-    "Type": "movie",
-    "DVD": "N/A",
-    "BoxOffice": "$193,735,288",
-    "Production": "N/A",
-    "Website": "N/A",
-    "Response": "True"
-},{
     "Title": "Men Suddenly in Black 2",
     "Year": "2006",
     "Rated": "N/A",
@@ -293,7 +254,7 @@ search()
 })
 
 function search(){
- fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=f6f4671c&s=${searchTerm.value}`)
+ fetch(`https://www.omdbapi.com/?apikey=f6f4671c&s=${searchTerm.value}`)
  .then(res => res.json())
  .then(data => {
     // OMDB returns a "Response" field as the string "True" or "False"
@@ -315,13 +276,13 @@ function getNames(SearchArray) {
  console.log(SearchArray.Search)
     for (let movie of SearchArray.Search) {
         //console.log(movie.Title)
-        movieList.push(movie.Title)
+        movieList.push(movie.imdbID)
     }
     console.log("the list of movies found: " + movieList)
     getMovies(movieList)
 }
 
-function getMovies(list){
+/* function getMovies(list){
     let detailedList = []
     console.log(list.length)
     for (let movie of list){
@@ -337,25 +298,58 @@ function getMovies(list){
 
     //populateList(detailedList)
 
+} */
+
+
+
+async function getMovies(list) {
+    console.log(list.length);
+    
+    const promises = [];
+    
+    // Loop through each movie and collect the promises
+    for (let movie of list) {
+        const promise = fetch(`https://www.omdbapi.com/?&apikey=f6f4671c&i=${movie}`)
+            .then(res => res.json())
+            .catch(err => null);
+        promises.push(promise);
+    }
+    
+    // Wait for all promises to complete
+    const detailedList = await Promise.all(promises);
+    
+    // Filter out failed responses
+    const validMovies = detailedList.filter(movie => movie.Response == "True" && movie.Actors !== "N/A");
+    
+    if (validMovies.length > 1){
+    populateList(validMovies);
+    } else {
+        document.getElementById("empty").innerHTML = "Unable to find what you're looking for. Please try another search."
+    }
 }
 
+
+
 function populateList(list){
+    // clear the list if its already there
+    document.getElementById("movie-list").innerHTML = ""
     console.log("called")
-    //console.log(list)
+    console.log(list)
+    console.log(list.length)
     document.getElementById("empty").style.display = "none"
     document.getElementById("movie-list").style.display = "block"
 
     for (let i = 0; i < list.length-1; i++){
       /*   Loop through the provided list and populate the movies 
       with adding a horizontal ruler under them (except the last one) */
-        console.log(list[i])
+
         document.getElementById("movie-list").innerHTML += 
         `    <div class="movie">
         <div class="movie-poster"><img src="${list[i].Poster}" class="poster"></div>
         <div class="movie-details">
             <div class="flex info1">
             <div class="movie-title">${list[i].Title}</div>
-            <div class="rating"><img src="images/star.svg" class="star">${list[i].Ratings[0].Value.slice(0, -3)}</div>
+            <div class="rating"><img src="images/star.svg" class="star">${list[i].Ratings[0].Value}</div>
             </div>
 
             <div class="movie-info flex">
@@ -369,7 +363,7 @@ function populateList(list){
     `
     }
             /* Populating the last item of the list */
-/*             document.getElementById("movie-list").innerHTML += 
+            document.getElementById("movie-list").innerHTML += 
         `    <div class="movie">
         <div class="movie-poster"><img src="${list[list.length-1].Poster}" class="poster"></div>
         <div class="movie-details">
@@ -385,7 +379,7 @@ function populateList(list){
             <div class="movie-plot">${list[list.length-1].Plot}</div>
         </div>
     </div>
-    ` */
+    `
 
 }
 
